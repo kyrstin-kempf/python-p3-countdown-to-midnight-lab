e345879247b2b262cf9fc5b15658847ae49af2bd
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-14 20:06:27 -0400

    created countdown while loops

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+import time
+
+def countdown(i):
+    while i <= 10:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+        if i == 0:
+            break
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(i):
+    while i <= 10:
+        print(f'{i} SECOND(S)!')
+        time.sleep(1)
+        i -= 1
+        if i == 0:
+            break
+    print("HAPPY NEW YEAR!")
